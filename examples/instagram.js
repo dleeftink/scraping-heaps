36d215543f7323af66ff0b54d58faa81b9b807bc
dleeftink <dennisleeftink@gmail.com>
2024-06-24 10:06:19 +0000

    Add parseHeapSnapshot.js

--- examples/instagram.js
+++ examples/instagram.js
@@ -1,19 +1,31 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const phs = require('puppeteer-heap-snapshot');
+const fs = require('fs');
 
-async function main() {
-    const browser = await puppeteer.launch({ 
-      headless:false
-    });
-    const page = await browser.newPage();
-    await page.goto("https://www.instagram.com/verge/");
-    const heapSnapshot = await phs.captureHeapSnapshot(page.target());
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const sleep = ms => new Promise(res => setTimeout(res, ms));
 
-    const objects = phs.findObjectsWithProperties(heapSnapshot, ["newStories"]);
-    console.log("Trying,", objects);
+puppeteer.use(StealthPlugin());
 
-    await browser.close();
+puppeteer.launch({ headless: true}).then(async browser => {
+  const page = await browser.newPage();
+  await page.goto("https://www.instagram.com/verge/p/C8PSn5zCeCm/");
+  await sleep(4600);
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
-}
+  const heapSnapshot = await phs.captureHeapSnapshot(page.target());
 
-main()
+  //const objects = phs.findObjectsWithProperties(heapSnapshot, ["newStories","timestamp"]);
+  //console.log("Trying,", objects);
+
+  // Serialize the heapSnapshot object to JSON
+  const serializedHeapSnapshot = JSON.stringify(heapSnapshot.strings, null, 2); // The second parameter (null) is for replacer function, third parameter (2) is for spacing
+ 
+  // Save the serialized heapSnapshot to a.json file
+  await fs.promises.writeFile('./data/heapSnapshotInstagram.json', serializedHeapSnapshot, (err) => {
+    if (err) throw err;
+    console.log('The heapSnapshot has been saved!');
+  });
+
+  await browser.close()
+})
--- examples/youtube.js
+++ examples/youtube.js
@@ -1,19 +1,34 @@
 const puppeteer = require('puppeteer');
 const phs = require('puppeteer-heap-snapshot');
 
+const fs = require('fs');
+
 async function main() {
-    const browser = await puppeteer.launch({ 
-      //executablePath: '/usr/bin/chromium-browser', args: ['--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox', '--no-zygote'] 
-      headless:false
-    });
-    const page = await browser.newPage();
-    await page.goto("https://www.youtube.com/watch?v=L_o_O7v1ews");
-    const heapSnapshot = await phs.captureHeapSnapshot(page.target());
+  const browser = await puppeteer.launch({
+    //executablePath: '/usr/bin/chromium-browser', args: ['--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox', '--no-zygote'] 
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.youtube.com/watch?v=L_o_O7v1ews");
+  
+  //await page.screenshot({path: 'screenshot.png'});
 
-    const objects = phs.findObjectsWithProperties(heapSnapshot, ["channelId", "viewCount","keywords"]);
-    console.log("Trying,", objects);
+  const heapSnapshot = await phs.captureHeapSnapshot(page.target());
+  
+  const objects = phs.findObjectsWithProperties(heapSnapshot, ["channelId", "viewCount", "keywords"]);
+  console.log("Trying,", objects);
 
-    await browser.close();
+  // Serialize the heapSnapshot object to JSON
+  /*const serializedHeapSnapshot = JSON.stringify(heapSnapshot.strings,null,2); // The second parameter (null) is for replacer function, third parameter (2) is for spacing
+
+  // Save the serialized heapSnapshot to a.json file
+  await fs.promises.writeFile('./data/heapSnapshotYoutube.json', serializedHeapSnapshot, (err) => {
+    if (err) throw err;
+    console.log('The heapSnapshot has been saved!');
+  });*/
+
+
+  await browser.close();
 
 }
 
