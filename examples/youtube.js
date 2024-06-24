e94d9aa8870f2a75060b28baeb8fa417728070eb
dleeftink <dennisleeftink@gmail.com>
2024-06-24 10:15:48 +0000

    Update youtube.js

--- examples/youtube.js
+++ examples/youtube.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 
 async function main() {
   const browser = await puppeteer.launch({
-    //executablePath: '/usr/bin/chromium-browser', args: ['--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox', '--no-zygote'] 
     headless: false
   });
   const page = await browser.newPage();
@@ -19,10 +18,10 @@ async function main() {
   console.log("Trying,", objects);
 
   // Serialize the heapSnapshot object to JSON
-  /*const serializedHeapSnapshot = JSON.stringify(heapSnapshot.strings,null,2); // The second parameter (null) is for replacer function, third parameter (2) is for spacing
+  //const serializedHeapSnapshot = JSON.stringify(heapSnapshot.strings/*,null,2*/); // The second parameter (null) is for replacer function, third parameter (2) is for spacing
 
   // Save the serialized heapSnapshot to a.json file
-  await fs.promises.writeFile('./data/heapSnapshotYoutube.json', serializedHeapSnapshot, (err) => {
+  /*await fs.promises.writeFile('./data/heapSnapshotYoutube.heapsnapshot', serializedHeapSnapshot, (err) => {
     if (err) throw err;
     console.log('The heapSnapshot has been saved!');
   });*/
